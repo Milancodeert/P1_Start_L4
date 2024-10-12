3f8af11902115b0558c0c9ab369bd632bf6794e8
Milancodeert <milan.stassen2012@gmail.com>
2024-10-12 10:39:00 +0200

    oefening 1+2

--- startcode/a_favoriete_dieren.py
+++ startcode/a_favoriete_dieren.py
@@ -3,3 +3,12 @@
 # - drie dieren vraagt aan de user
 # - deze dieren toevoegt aan de lijst
 # - de lijst met dieren toont aan de user
+favoDieren = []
+favorietDier1 =(input("Ik ga je vragen wat je lievelingsdieren zijn! Wat is je 3de lievelingsdier?"))
+favorietDier2 =(input("je 2de lievelingsdier?"))
+favorietDier3 =(input("Je lievelingsdier allertijden?"))
+favoDieren.append(favorietDier1)
+favoDieren.append(favorietDier2)
+favoDieren.append(favorietDier3)
+print(favoDieren)
+
--- startcode/b_vind_grootste_getal.py
+++ startcode/b_vind_grootste_getal.py
@@ -1,3 +1,27 @@
 # Maak een lijst met getallen.
 # Schrijf een functie vind_grootste_getal die de grootste waarde uit een lijst teruggeeft.
 # Gebruik een for-loop om de grootste waarde te vinden.
+
+getallenLijst = []
+Cijfer1 =(input("Ik ga je vragen om 5 getallen te typen! begin met een cijfer te typen! "))
+Cijfer2 =(input("Nu een ander cijfer! "))
+Cijfer3 =(input("Nog een ander cijfer! "))
+Cijfer4 =(input("Nog een ander cijfer! "))
+Cijfer5 =(input("Nog een ander cijfer! "))
+getallenLijst.append(Cijfer1)
+getallenLijst.append(Cijfer2)
+getallenLijst.append(Cijfer3)
+getallenLijst.append(Cijfer4)
+getallenLijst.append(Cijfer5)
+
+def vind_grootste_getal_traag(getallenLijst):
+    if(len(getallenLijst)<1):
+        print("list too short")
+        getallenLijst.sort()
+        print(getallenLijst[0])
+
+
+
+
+
+
